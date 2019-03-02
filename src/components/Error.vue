44131d88dddd1238ce2f3b3da6507ee19c2018a7
Bilie <bilianavaleva@gmail.com>
2019-03-02 15:47:08 +0100

    Add error component

---
+++ src/components/Error.vue
@@ -0,0 +1,30 @@
+<template>
+  <p
+    class="error"
+    :class="size ? 'error--' + size : 'indented-content-box'">
+    Something went wrong, please, try again later.
+  </p>
+</template>
+
+<script>
+  export default {
+    name: "Error",
+    props: {
+      size: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .error {
+    font-size: 32px;
+  }
+
+  .error--small {
+    margin: 8px 0;
+    font-size: 16px;
+  }
+</style>
