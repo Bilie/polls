ea3a51b1893f96565d2213560f8cc35295a49ed8
Bilie <bilianavaleva@gmail.com>
2019-03-02 12:21:18 +0100

    Add normalize.scss

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 </template>
 
 <style lang="scss">
+@import "~normalize-scss/sass/normalize/_import-now.scss";
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
