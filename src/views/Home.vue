5411ceadcab2837f5b026e010952a93e6fa2ed39
Bilie <bilianavaleva@gmail.com>
2019-03-03 13:59:01 +0100

    Add button to new new questions page on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,21 @@
 <template>
   <section class="page">
-    <h1 class="page__headline">Polls</h1>
-    <p v-if="!loading">Select a poll to vote</p>
-    <template v-if="!loading && !hasError">
-      <QuestionsList :data="questions" />
-    </template>
-    <template v-if="hasError"
-      ><Error
-    /></template>
-    <section v-if="loading">Loading...</section>
+      <section class="page__header">
+        <section>
+          <h1 class="page__headline">Polls</h1>
+          <p v-if="!loading">Select a poll to vote</p>
+        </section>
+
+        <router-link class="button button--secondary" to="/questions/new">New poll</router-link>
+      </section>
+
+      <template v-if="!loading && !hasError">
+        <QuestionsList :data="questions" />
+      </template>
+      <template v-if="hasError"
+        ><Error
+      /></template>
+      <section v-if="loading">Loading...</section>
   </section>
 </template>
 
